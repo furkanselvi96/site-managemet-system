5ae761b0b2dc21c418d1e6b5dae5f94a7f50b139
furkanselvi <furkan-selvi-1996@hotmail.com>
2023-08-12 14:11:20 +0300

    Initial Project

---
+++ src/main/java/com/sms/sitemanagemetsystem/SiteManagemetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sms.sitemanagemetsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteManagemetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteManagemetSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sms/sitemanagemetsystem/SiteManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.sitemanagemetsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
